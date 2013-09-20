15855dafc861c5cec6335d878d2071add7f47fd2
Ryan LeFevre <meltingice8917@gmail.com>
2013-09-20 16:55:30 -0400

    First working version

--- ext/psd_native/image_mode_rgb.c
+++ ext/psd_native/image_mode_rgb.c
@@ -6,34 +6,25 @@ VALUE psd_native_combine_rgb_channel(VALUE self) {
   uint32_t num_pixels = FIX2UINT(rb_iv_get(self, "@num_pixels"));
   uint32_t pixel_step = FIX2UINT(rb_funcall(self, rb_intern("pixel_step"), 0));
 
-  // printf("PIXELS = %d, STEP = %d\n", num_pixels, pixel_step);
-
   VALUE channels_info = rb_iv_get(self, "@channels_info");
   VALUE channel_data = rb_iv_get(self, "@channel_data");
   uint32_t channel_length = FIX2UINT(rb_iv_get(self, "@channel_length"));
+  uint32_t channel_count = FIX2UINT(rb_funcall(channels_info, rb_intern("length"), 0));
 
-  // printf("CHANNELS = %d\n", channel_length);
-  
-  VALUE channel;
-
-  int i, j, r, g, b, a;
-  uint32_t val;
+  int i, j;
+  uint32_t val, r, g, b, a;
 
   // Loop through every pixel in the image
   for (i = 0; i < num_pixels; i += pixel_step) {
     r = g = b = 0;
     a = 255;
 
-    // printf("%d\n", i);
-
     // And every channel for every pixel
-    for (j = 0; j < channel_length; j++) {
+    for (j = 0; j < channel_count; j++) {
       val = FIX2UINT(rb_ary_entry(channel_data, i + (channel_length * j)));
-      // printf("%d\n", val);
 
       // Get the hash containing channel info
-      channel = rb_ary_entry(channels_info, j);
-      switch (FIX2INT(rb_hash_aref(channel, rb_intern("id")))) {
+      switch (FIX2INT(rb_hash_aref(rb_ary_entry(channels_info, j), ID2SYM(rb_intern("id"))))) {
         case -1: a = val; break;
         case 0:  r = val; break;
         case 1:  g = val; break;
