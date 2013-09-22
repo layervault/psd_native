32d419e4ec9ce5a30b73d49f98e22be1d62b380d
Ryan LeFevre <meltingice8917@gmail.com>
2013-09-22 18:34:43 -0400

    Switch to pointers instead of pulling in a ton of data

--- ext/psd_native/rle_decoding.c
+++ ext/psd_native/rle_decoding.c
@@ -1,56 +1,52 @@
 #include "psd_native_ext.h"
 
 VALUE psd_native_decode_rle_channel(VALUE self) {
-  int height = FIX2INT(rb_funcall(self, rb_intern("height"), 0));
-  VALUE channel_data = rb_iv_get(self, "@channel_data");
   int bytes, finish, len, val;
 
+  int height = FIX2INT(rb_funcall(self, rb_intern("height"), 0));
+  VALUE* channel_data = RARRAY_PTR(rb_iv_get(self, "@channel_data"));
+  VALUE* byte_counts = RARRAY_PTR(rb_iv_get(self, "@byte_counts"));
+  int line_index = FIX2INT(rb_iv_get(self, "@line_index"));
+  int chan_pos = FIX2INT(rb_iv_get(self, "@chan_pos"));
+
   for (int i = 0; i < height; i++) {
-    bytes = psd_byte_count(self, i);
+    bytes = FIX2INT(byte_counts[line_index + i]);
     finish = psd_file_tell(self) + bytes;
 
-    while (psd_file_tell(self) < finish) {
+    for (int j = 0; j < bytes;) {
       len = FIX2INT(psd_file_read_byte(self));
+      j++;
 
       if (len < 128) {
         len++;
-        for (int j = psd_chan_pos(self); j < psd_chan_pos(self) + len; j++) {
-          rb_ary_store(channel_data, j, psd_file_read_byte(self));
+        for (int j = chan_pos; j < chan_pos + len; j++) {
+          channel_data[j] = psd_file_read_byte(self);
         }
 
-        rb_iv_set(self, "@chan_pos", INT2FIX(psd_chan_pos(self) + len));
+        chan_pos += len;
+        j += len;
       } else if (len > 128) {
         len ^= 0xff;
         len += 2;
 
         val = psd_file_read_byte(self);
-        for (int j = psd_chan_pos(self); j < psd_chan_pos(self) + len; j++) {
-          rb_ary_store(channel_data, j, val);
+        for (int j = chan_pos; j < chan_pos + len; j++) {
+          channel_data[j] = val;
         }
 
-        rb_iv_set(self, "@chan_pos", INT2FIX(psd_chan_pos(self) + len));
+        chan_pos += len;
+        j++;
       }
     }
+
+    rb_iv_set(self, "@chan_pos", INT2FIX(chan_pos));
   }
 
   return Qnil;
-}
-
-int psd_byte_count(VALUE self, int line) {
-  return FIX2INT(
-    rb_ary_entry(
-      rb_iv_get(self, "@byte_counts"), 
-      FIX2INT(rb_iv_get(self, "@line_index")) + line
-    )
-  );
 }
 
 VALUE psd_file_read_byte(VALUE self) {
   VALUE data = rb_funcall(psd_file(self), rb_intern("read"), 1, INT2FIX(1));
   VALUE bytes = rb_funcall(data, rb_intern("bytes"), 0);
   return rb_ary_entry(rb_funcall(bytes, rb_intern("to_a"), 0), 0);
-}
-
-int psd_chan_pos(VALUE self) {
-  return FIX2INT(rb_iv_get(self, "@chan_pos"));
 }
--- ext/psd_native/rle_decoding.h
+++ ext/psd_native/rle_decoding.h
@@ -2,8 +2,6 @@
 #define PSD_NATIVE_RLE_DECODING
 
 VALUE psd_native_decode_rle_channel(VALUE self);
-int psd_byte_count(VALUE self, int line);
 VALUE psd_file_read_byte(VALUE self);
-int psd_chan_pos(VALUE self);
 
 #endif
