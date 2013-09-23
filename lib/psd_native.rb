fd7f21c5d36852ff166fe0a0eab47134448844e2
Ryan LeFevre <meltingice8917@gmail.com>
2013-09-22 21:10:03 -0400

    Remove old note

--- lib/psd_native.rb
+++ lib/psd_native.rb
@@ -5,8 +5,6 @@ require "psd_native/psd_native"
 module PSDNative
   def self.included(base)
     base::Image.send(:include, PSDNative::ImageMode::RGB)
-
-    # Disabled until optimized
     base::Image.send(:include, PSDNative::ImageFormat::RLE)
   end
 end
