d66df73341ad6e8ce74ea651fe3a6935c0197290
Ryan LeFevre <meltingice8917@gmail.com>
2013-09-22 20:13:16 -0400

    Add tests

--- psd_native.gemspec
+++ psd_native.gemspec
@@ -25,4 +25,13 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rake-compiler"
+
+  spec.test_files = Dir.glob("spec/**/*")
+  spec.add_development_dependency 'rspec'
+  spec.add_development_dependency 'guard'
+  spec.add_development_dependency 'guard-rspec'
+
+  if RUBY_PLATFORM =~ /darwin/
+    spec.add_development_dependency 'rb-fsevent', '~> 0.9'
+  end
 end
---
+++ spec/psd_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'PSD' do
+  let(:filename) { 'spec/files/example.psd' }
+
+	it 'should open a file without a block' do
+    psd = PSD.open(filename)
+    expect(psd).to be_parsed
+    expect(psd).to be_an_instance_of(PSD)
+	end
+
+  it 'should refuse to open a bad filename' do
+    expect { PSD.open('') }.to raise_error
+  end
+
+  it 'should open a file and feed it to a block' do
+    PSD.open(filename) do |psd|
+      expect(psd).to be_parsed
+      expect(psd).to be_an_instance_of(PSD)
+    end
+  end
+
+  # We have to use #should syntax here because the DSL binds
+  # the block to the PSD instance.
+  it 'should open a file and feed it to a block DSL style' do
+    PSD.open(filename) do
+      parsed?.should == true
+      is_a?(PSD).should == true
+    end
+  end
+end
