e9f86fa46a6ff1934cb3d4108e66d937f02d72cb
Ryan LeFevre <meltingice8917@gmail.com>
2013-11-19 23:44:26 -0500

    More speed improvements for RGB processing

--- ext/psd_native/image_mode_rgb.c
+++ ext/psd_native/image_mode_rgb.c
@@ -12,32 +12,33 @@ VALUE psd_native_combine_rgb_channel(VALUE self) {
   int channel_count = RARRAY_LENINT(rb_iv_get(self, "@channels_info"));
 
   int i, j;
-  uint32_t val, r, g, b, a;
+  uint32_t val, color;
 
   int channel_ids[channel_count];
   for (i = 0; i < channel_count; i++) {
-    channel_ids[0] = FIX2INT(rb_hash_aref(channels_info[i], ID2SYM(rb_intern("id"))));
+    channel_ids[i] = FIX2INT(rb_hash_aref(channels_info[i], ID2SYM(rb_intern("id"))));
   }
 
   // Loop through every pixel in the image
   for (i = 0; i < num_pixels; i += pixel_step) {
-    r = g = b = 0;
-    a = 255;
+    color = 0x000000ff;
 
     // And every channel for every pixel
     for (j = 0; j < channel_count; j++) {
+      if (channel_ids[j] == -2) continue;
+
       val = FIX2UINT(channel_data[i + (channel_length * j)]);
 
       // Get the hash containing channel info
       switch (channel_ids[j]) {
-        case -1: a = val; break;
-        case 0:  r = val; break;
-        case 1:  g = val; break;
-        case 2:  b = val; break;
+        case -1: color = (color & 0xffffff00) | val; break;         // A
+        case 0:  color = (color & 0x00ffffff) | (val << 24); break; // R
+        case 1:  color = (color & 0xff00ffff) | (val << 16); break; // G
+        case 2:  color = (color & 0xffff00ff) | (val << 8); break;  // B
       }
     }
 
-    rb_ary_push(rb_iv_get(self, "@pixel_data"), INT2FIX(BUILD_PIXEL(r, g, b, a)));
+    rb_ary_push(rb_iv_get(self, "@pixel_data"), INT2FIX(color));
   }
 
   return Qnil;
