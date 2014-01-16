139a528ad03190e6ca717bf71a33b11fc408c0a5
Ryan LeFevre <meltingice8917@gmail.com>
2014-01-16 14:41:09 -0500

    Small improvement to RLE decoding times

--- ext/psd_native/rle_decoding.c
+++ ext/psd_native/rle_decoding.c
@@ -2,13 +2,15 @@
 
 VALUE psd_native_decode_rle_channel(VALUE self) {
   int bytes, len, val;
-  int i, j, k;
+  int i, j, k, l;
 
   int height = FIX2INT(rb_funcall(self, rb_intern("height"), 0));
   VALUE channel_data = rb_iv_get(self, "@channel_data");
   VALUE* byte_counts = RARRAY_PTR(rb_iv_get(self, "@byte_counts"));
   int line_index = FIX2INT(rb_iv_get(self, "@line_index"));
   int chan_pos = FIX2INT(rb_iv_get(self, "@chan_pos"));
+
+  VALUE byte_data;
 
   for (i = 0; i < height; i++) {
     bytes = FIX2INT(byte_counts[line_index + i]);
@@ -19,8 +21,11 @@ VALUE psd_native_decode_rle_channel(VALUE self) {
 
       if (len < 128) {
         len++;
+
+        l = 0;
+        byte_data = psd_file_read_bytes(self, len);
         for (k = chan_pos; k < chan_pos + len; k++) {
-          rb_ary_store(channel_data, k, psd_file_read_byte(self));
+          rb_ary_store(channel_data, k, rb_ary_entry(byte_data, l++));
         }
 
         chan_pos += len;
