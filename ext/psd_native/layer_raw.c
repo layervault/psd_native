877120c9cb9f962f3ea37c87f6c9e72d687f350e
Ryan LeFevre <meltingice8917@gmail.com>
2014-02-03 15:05:12 -0500

    Fix RAW data export

--- ext/psd_native/layer_raw.c
+++ ext/psd_native/layer_raw.c
@@ -7,11 +7,11 @@ VALUE psd_native_layer_raw_parse_raw_bang(VALUE self) {
   int chan_length = FIX2INT(rb_hash_aref(rb_iv_get(self, "@ch_info"), ID2SYM(rb_intern("length"))));
   VALUE channel_data = rb_iv_get(self, "@channel_data");
 
-  VALUE *data = RARRAY_PTR(psd_file_read_bytes(self, chan_length - 2));
+  VALUE data = psd_file_read_bytes(self, chan_length - 2);
 
   int i, j = 0;
   for (i = chan_pos; i < (chan_pos + chan_length - 2); i++) {
-    rb_ary_store(channel_data, i, data[j++]);
+    rb_ary_store(channel_data, i, rb_ary_entry(data, j++));
   }
 
   rb_iv_set(self, "@chan_pos", INT2FIX(chan_pos + chan_length - 2));
