211ac47767e338e0cd08afc1b63adfe79e3f70f9
Ryan LeFevre <meltingice8917@gmail.com>
2014-03-07 11:41:50 -0500

    Fix specs

--- spec/psd_spec.rb
+++ spec/psd_spec.rb
@@ -3,11 +3,17 @@ require 'spec_helper'
 describe 'PSD' do
   let(:filename) { 'spec/files/example.psd' }
 
-	it 'should open a file without a block' do
-    psd = PSD.open(filename)
-    expect(psd).to be_parsed
+  it 'should open a file without a block' do
+    psd = PSD.new(filename)
+    expect(psd).to_not be_parsed
     expect(psd).to be_an_instance_of(PSD)
-	end
+  end
+
+  it 'should raise an exception if using open without a block' do
+    expect {
+      PSD.open(filename)  
+    }.to raise_error
+  end
 
   it 'should refuse to open a bad filename' do
     expect { PSD.open('') }.to raise_error
